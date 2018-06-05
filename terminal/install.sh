63f8f836544761a506e55fa0f321b0136d2872a7
Martin Chalupa <mchalupa@netflix.com>
2018-06-05 09:49:41 -0700

    Clones for oh-my-zsh plugins

---
+++ terminal/install.sh
@@ -0,0 +1,3 @@
+#clone repos for oh my zsh plugins
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
