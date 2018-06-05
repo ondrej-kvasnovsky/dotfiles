42d2dd6580875678b2283a62f5f774ff41507534
Martin Chalupa <mchalupa@netflix.com>
2018-06-05 12:48:48 -0700

    Correct spacing for plugin configuration

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,3 +1,3 @@
 #clone repos for oh my zsh plugins
-sudo git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
